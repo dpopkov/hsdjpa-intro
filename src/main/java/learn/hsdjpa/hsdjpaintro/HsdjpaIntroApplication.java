ab3a9569b4fa95a90893b1ed4d22cb3c44b08195
Denis Popkov <pkvdenis@gmail.com>
2022-01-19 17:05:29 +0300

    Add base Spring Boot project.

---
+++ src/main/java/learn/hsdjpa/hsdjpaintro/HsdjpaIntroApplication.java
@@ -0,0 +1,13 @@
+package learn.hsdjpa.hsdjpaintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsdjpaIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsdjpaIntroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learn/hsdjpa/hsdjpaintro/HsdjpaIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.hsdjpa.hsdjpaintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsdjpaIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
